d62aabcffe5d0e720daab9635c8ecd8e4c315e01
dev-yoonjung <dear__yoon@naver.com>
2022-06-14 16:01:22 +0900

    chore: create a spring boot project
    Created a spring-boot project. This project was created for the configuration lettuce with redis standalone.

---
+++ src/main/java/lettuce/redis/standalone/StandaloneApplication.java
@@ -0,0 +1,13 @@
+package lettuce.redis.standalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandaloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StandaloneApplication.class, args);
+    }
+
+}
